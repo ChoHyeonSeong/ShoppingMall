2e64fb9653ebfd084f06e94fbb0eacf17459e4f6
johs <johs970917@gmail.com>
2024-03-29 22:23:57 +0900

    js파일 생성 및 함수 네이밍

---
+++ ShoppingMall/src/main/webapp/sm_js2.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+let parent = $('main');
+let child = $('<div', {
+	class: 'prod-box radius-box'
+});
+
+function createProduct(src, title, writer, summary) {
+	let product;
+	return product;
+}
+
+$(document).ready(() => {
+
+	/*	$(".gardenimg").on("mouseenter", function() {
+			let rand = parseInt(Math.random() * 500);
+			$(this).append("<p>방문자수:" + rand + "</p>");
+		});
+		$(".gardenimg").on("mouseleave", function() {
+			$(this).children().last().remove();
+		});*/
+});
