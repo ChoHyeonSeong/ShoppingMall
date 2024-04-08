30ce9f4f64a6b561f4056fb24bd5cfa7d84297f7
johs <johs970917@gmail.com>
2024-04-08 22:56:55 +0900

    데이터 하나의 html에 몰기

--- ShoppingMall/src/main/webapp/front/js/sm_fiction_js.js
+++ ShoppingMall/src/main/webapp/front/js/sm_fiction_js.js
@@ -13,15 +13,23 @@ function addProduction(prods) {
 	if (prods.length > 0) {
 		// img 주소, 제목, 작가 총 3개 요소
 		if (prods[0].length > 0 && prods[0].length < 4) {
-			let prodBox = $('.prod-box');
-			let prod = prods[0];
-			changeContent(prodBox, prod[0], prod[1], prod[2]);
-			for (let i = 1; i < prods.length; i++) {
-				prod = prods[i];
-				let box = prodBox.clone();
-				changeContent(box, prod[0], prod[1], prod[2]);
-				$('main').append(box);
-			}
+
+			$.get("production.html", function(data) {
+				let prodBox = $('.prod-box');
+				let prod = prods[0];
+				changeContent(prodBox, prod[0], prod[1], prod[2]);
+				for (let i = 1; i < prods.length; i++) {
+					prod = prods[i];
+					let box = prodBox.clone();
+					changeContent(box, prod[0], prod[1], prod[2]);
+					$('main').append(box);
+				}
+			});
 		}
 	}
+}
+
+
+function addGenre(genre,) {
+
 }
