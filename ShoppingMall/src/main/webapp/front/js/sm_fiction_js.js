134732d08c6033dce0c83847d48eebc25bd74a6f
johs <johs970917@gmail.com>
2024-04-16 00:32:09 +0900

    장바구니 넣기/빼기 스타일 구현

--- ShoppingMall/src/main/webapp/front/js/sm_fiction_js.js
+++ ShoppingMall/src/main/webapp/front/js/sm_fiction_js.js
@@ -11,21 +11,3 @@ function changeContent(box, src, title, writer) {
 	box.find('.fiction-writer').text(writer);
 }
 
-function addProduction(prods) {
-	if (prods.length > 0) {
-		// img 주소, 제목, 작가 총 3개 요소
-		if (prods[0].length > 0 && prods[0].length < 4) {
-
-			$.get(productionPath, function(data) {
-				let prodBox = $(data);
-				let prod = prods[0];
-				for (let i = 0; i < prods.length; i++) {
-					prod = prods[i];
-					let box = prodBox.clone();
-					changeContent(box, imgPath + prod[0], prod[1], prod[2]);
-					$('main').append(box);
-				}
-			});
-		}
-	}
-}
